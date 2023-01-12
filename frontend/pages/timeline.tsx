0e5ae4eb41226e0a0573a454b743d98cb09fbe19
Mihaela M <mihaelamihaiu99@gmail.com>
2023-01-13 00:39:48 +0200

    Added output examples

--- frontend/pages/timeline.tsx
+++ frontend/pages/timeline.tsx
@@ -1,9 +1,12 @@
+import ToggleButton from '@mui/material/ToggleButton'
+import ToggleButtonGroup from '@mui/material/ToggleButtonGroup'
 import { useRouter } from 'next/router'
 import { useEffect, useState } from 'react'
 import { ImageDisplay } from '../components/ImageDisplay'
 import Loading from '../components/Loading'
 import { TimeSlider } from '../components/TimeSlider'
 import styles from '../styles/Timeline.module.css'
+import CheckIcon from '@mui/icons-material/Check'
 
 interface ImageDetails {
   id: string // jobId, get status from another api similar to tts azure
@@ -20,16 +23,47 @@ export default function TimelinePage () {
 
   const [dates, setDates] = useState<Array<ImageDetails> | undefined>(undefined)
   const [currentImage, setCurrentImage] = useState('')
+  const [viewMode, setViewMode] = useState<string | null>('Processed')
+  const [toggledNDVI, setToggledNDVI] = useState<boolean>(true)
+  const [toggledLegend, setToggledLegend] = useState<boolean>(true)
+  const [sliderValue, setSliderValue] = useState<number>(0)
+
+  let getNDVIName: (date: Date) => string = function (date: Date): string {
+    var out: string
+    out =
+      'images/' +
+      date.getFullYear().toString() +
+      (date.getMonth() + 1).toString() +
+      date.getDate().toString() +
+      '.png'
+    console.log(out, date)
+    return out
+  }
+
+  let getSatelliteName: (date: Date) => string = function (date: Date): string {
+    var out: string
+    out =
+      'images/color_' +
+      date.getFullYear().toString() +
+      (date.getMonth() + 1).toString() +
+      date.getDate().toString() +
+      '.png'
+    console.log(out, date)
+    return out
+  }
 
   useEffect(() => {
-    let date1 = { id: '1', dateTaken: new Date('2022-06-13') }
-    let date2 = { id: '2', dateTaken: new Date('2021-06-13') }
-    let date3 = { id: '3', dateTaken: new Date('2021-07-13') }
-    setDates([date1, date2, date3])
-    setCurrentImage(
-      'https://cdn.britannica.com/25/160325-050-EB1C8FB7/image-instruments-Earth-satellite-NASA-Suomi-National-2012.jpg'
-    )
-    // slider is not firing its first position
+    let dates = [
+      { id: '1', dateTaken: new Date('2017-06-30') },
+      { id: '2', dateTaken: new Date('2018-05-31') },
+      { id: '3', dateTaken: new Date('2019-08-29') },
+      { id: '4', dateTaken: new Date('2020-07-29') },
+      { id: '5', dateTaken: new Date('2021-07-14') },
+      { id: '6', dateTaken: new Date('2022-06-29') }
+    ]
+    setDates(dates)
+    setCurrentImage(getNDVIName(dates[0].dateTaken))
+    setSliderValue(dates[0].dateTaken.getTime())
   }, [])
 
   function sliderChanged (
@@ -38,27 +72,83 @@ export default function TimelinePage () {
     _activeThumb: number
   ) {
     var link: string
-    console.log(value)
-    if (value == 1623542400000)
-      link =
-        'https://cdn.britannica.com/25/160325-050-EB1C8FB7/image-instruments-Earth-satellite-NASA-Suomi-National-2012.jpg'
-    else if (value == 1655078400000)
-      link = 'https://www.gstatic.com/earth/social/00_generic_facebook-001.jpg'
-    else
-      link =
-        'https://www.worldatlas.com/r/w1300-q80/upload/7d/db/c3/shutterstock-1558058690.jpg'
+    if (toggledNDVI) link = getNDVIName(new Date(Number(value)))
+    else link = getSatelliteName(new Date(Number(value)))
+
+    console.log(link)
+
     if (link !== currentImage) {
+      setSliderValue(Number(value))
       setCurrentImage(link)
       console.log(link)
     }
   }
 
+  const handleViewMode = (
+    event: React.MouseEvent<HTMLElement>,
+    newAlignment: string | null
+  ) => {
+    setViewMode(newAlignment)
+    if (toggledNDVI) setCurrentImage(getSatelliteName(new Date(sliderValue)))
+    else setCurrentImage(getNDVIName(new Date(sliderValue)))
+    setToggledNDVI(!toggledNDVI)
+  }
+
   return (
     <div>
       <div>{!dates && <Loading />}</div>
-      <div className={styles.imagedisplay}>
-        {dates && <ImageDisplay source={currentImage} />}
-      </div>
+      {dates && (
+        <div className={styles.imagedisplay}>
+          <div style={{ position: 'fixed', left: '5vw', top: '1vh' }}>
+            <ToggleButtonGroup
+              value={viewMode}
+              exclusive
+              onChange={handleViewMode}
+              style={{ backgroundColor: 'rgba(240, 255, 255, 0.4)' }}
+            >
+              <ToggleButton value='Processed'>Processed</ToggleButton>
+              <ToggleButton value='Satellite'>Satellite</ToggleButton>{' '}
+            </ToggleButtonGroup>
+          </div>
+          {toggledNDVI && (
+            <div
+              style={{
+                backgroundColor: 'rgba(240, 255, 255, 0.4)',
+                position: 'fixed',
+                right: '5vw',
+                top: '1vh',
+                padding: '5px',
+                display: 'flex',
+                color: 'black',
+                borderRadius: '10px'
+              }}
+            >
+              <h3 style={{ margin: '10px' }}>Show legend</h3>
+              <ToggleButton
+                value='check'
+                selected={toggledLegend}
+                onChange={() => {
+                  setToggledLegend(!toggledLegend)
+                }}
+              >
+                <CheckIcon />
+              </ToggleButton>
+            </div>
+          )}
+          {toggledNDVI && toggledLegend && (
+            <img
+              style={{
+                position: 'fixed',
+                height: '100px',
+                left: '5vw',
+                top: '10vh'
+              }}
+              src='images/legend.png'
+            ></img>
+          )}
+          <ImageDisplay source={currentImage} />
+        </div>
+      )}
       <div className={styles.timeslider}>
         <TimeSlider
           dates={dates?.map(d => d.dateTaken)}
